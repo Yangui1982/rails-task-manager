a25a53e8a17dbd9b2b79022f331402aea28706a6
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-05-31 13:00:08 +0200

    little changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new',  as: :new_task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
 
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
